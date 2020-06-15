5797a45c431021c374b1e0051b42aa48b43e988a
Hawk Dykes <hawkdykes@yahoo.com>
2020-06-14 23:17:10 -0500

    second set of functionalities -- todo: reset, dims

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-let dim = 16;
+let dim = 20;
+let color = false;
 
 const container = document.querySelector('.container');
 
@@ -14,12 +15,41 @@ let pixels = document.querySelectorAll('.pixel');
 
 pixels.forEach((pixel) => {
   pixel.addEventListener("mouseover", function(event) {
-    event.target.style.backgroundColor = randomColor();/*function () {
-      console.log('here');
-      let color = randomColor();
-      console.log(color);
-      return color;
-    } */
+    if (!color) {
+      event.target.style.backgroundColor = 'black';
+    }
+    else {
+      event.target.style.backgroundColor = randomColor();
+    }
   })
 })
 
+const clearBtn = document.querySelector('#clear');
+
+clearBtn.addEventListener('click', function(event) {
+  pixels.forEach((pixel) => {
+    pixel.style.backgroundColor = 'white';
+  })
+})
+const colorBtn = document.querySelector('#color');
+
+colorBtn.addEventListener('click', function(event) {
+  color = !color;
+  if (color) {
+    event.target.style.backgroundImage = 
+        'linear-gradient(62deg, rgb(255, 0, 0) 0%, rgb(0, 255, 0) 50%, rgb(0, 0, 255) 100%)';
+  }
+  else if (!color) {
+    event.target.style.backgroundImage = 
+        'linear-gradient(62deg, #3A3d40 0%, #181719 100%)';
+  }
+})
+
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
